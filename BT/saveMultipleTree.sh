9b00f0d0837f7e98f2d908e097b995abdd5767df
Xiaozhou Yu <xzyu18@fudan.edu.cn>
2025-12-30 12:04:51 +0800

    update ipynb for cumulant

--- BT/saveMultipleTree.sh
+++ BT/saveMultipleTree.sh
@@ -1,3 +1,7 @@
+# for alpha in 2.0 3.0 4.0; do
+#   root -l -b -q "saveTreeAA.C(\"Au197pnHFB14\",\"Au197pnHFB14\",\"200\",${alpha},0,5)"
+# done
+
 for alpha in 2.0 3.0 4.0; do
-  root -l -b -q "saveTreeAA.C(\"Au197pnHFB14\",\"Au197pnHFB14\",\"200\",${alpha},0,5)"
+  root -l -b -q "saveTreeAA.C(\"Pbpnrw\",\"Pbpnrw\",\"17p3\",${alpha},0,5)"
 done
